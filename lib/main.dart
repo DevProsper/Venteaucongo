ef5149037dbcca8abe166d2782581c366a5bef8d
Prosper NGOUARI <prosperngouari38@gmail.com>
2019-04-19 12:52:17 -0700

    Finition du Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -26,8 +26,82 @@ class _HomePageState extends State<HomePage>{
           backgroundColor: Colors.red,
           title: Text("Venteaucongo"),
           actions: <Widget>[
-            new IconButton(icon: Icon(Icons.search, color: Colors.white,), onPressed: (){})
+            new IconButton(icon: Icon(Icons.search, color: Colors.white,), onPressed: (){}),
+            new IconButton(icon: Icon(Icons.shopping_cart, color: Colors.white,), onPressed: (){})
           ],
+      ),
+
+      drawer: new Drawer(
+          child: ListView(
+              children: <Widget>[
+                //header
+                new UserAccountsDrawerHeader(
+                    accountName: Text("Prosper NGOUARI"),
+                    accountEmail: Text("ngprosper@asnect-it.com"),
+                    currentAccountPicture: GestureDetector(
+                        child: new CircleAvatar(
+                            backgroundColor: Colors.grey,
+                          child: Icon(Icons.person, color: Colors.white),
+                        ),
+                    ),
+                    decoration: new BoxDecoration(
+                      color: Colors.red
+                    ),
+                ),
+
+                //body
+
+                InkWell(
+                  onTap: (){},
+                  child: ListTile(
+                    title: Text('Accueil'),
+                    leading: Icon(Icons.home),
+                  ),
+                ),
+
+                InkWell(
+                  onTap: (){},
+                  child: ListTile(
+                    title: Text('Mes commandes'),
+                    leading: Icon(Icons.shopping_basket),
+                  ),
+                ),
+
+                InkWell(
+                  onTap: (){},
+                  child: ListTile(
+                    title: Text('Categories'),
+                    leading: Icon(Icons.dashboard),
+                  ),
+                ),
+
+                InkWell(
+                  onTap: (){},
+                  child: ListTile(
+                    title: Text('Mes favories'),
+                    leading: Icon(Icons.favorite),
+                  ),
+                ),
+
+                Divider(),
+
+                InkWell(
+                  onTap: (){},
+                  child: ListTile(
+                    title: Text('Parametres'),
+                    leading: Icon(Icons.settings),
+                  ),
+                ),
+
+                InkWell(
+                  onTap: (){},
+                  child: ListTile(
+                    title: Text('A propos'),
+                    leading: Icon(Icons.help),
+                  ),
+                ),
+              ],
+          ),
       ),
     );
   }
